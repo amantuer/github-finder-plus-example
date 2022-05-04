8a529d042c0ed4d5c15e579abc573aee62ff0faa
tuerfight <tuerfight@users.noreply.github.com>
2022-05-04 10:11:37 +0900

    fix css of search bar

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -71,7 +71,7 @@ function UserSearch(){
     }
   }
 
-  return <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+  return <div id="search-bar" className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
      <div>
       <form onSubmit={handleSubmit}>
        <div className="form-control">
